d1a6cfeee9bb151857ef2fef00e79bdcc070cb01
Matheo Delaunay <matheo.delaunay@adeo.com>
2023-05-10 00:04:08 +0200

    feat/init_web3_connexion

---
+++ app/src/components/admin/TallyVote.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const tallyVote = () => {
+
+}
+</script>
+
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <button class="btn btn-primary" v-on:click="tallyVote">Tally votes</button>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
