0301bb0467fb3a6d34d3aaf00cda8b239b919f81
Matheo Delaunay <matheo.delaunay@adeo.com>
2023-05-10 00:04:08 +0200

    feat/init_web3_connexion

---
+++ app/src/components/admin/ShowProposal.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.js";
+
+const finishProposal = () => {
+
+}
+
+const emit = defineEmits<{
+    (e:'endProposal'):void
+}>()
+</script>
+
+<template>
+    <div class="container">
+        <div class="row my-5">
+            <ul class="list-group">
+                <li class="list-group-item">test1</li>
+                <li class="list-group-item">test2</li>
+            </ul>
+        </div>
+        <div class="row justify-content-center">
+            <button class="btn btn-primary" v-on:click="finishProposal">Finish proposals</button>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ app/src/components/admin/StartVote.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.js";
+
+const startVote = () => {
+
+}
+</script>
+
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <button class="btn btn-primary" v-on:click="startVote">Start vote</button>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
